8bccb0810c4a32d0d2e0f1a17d0e31989d14d12e
Yushan Han <shelly.y.han@gmail.com>
2022-03-18 08:35:19 -0700

    init

---
+++ src/main/kotlin/big/Application.kt
@@ -0,0 +1,13 @@
+package big
+
+import io.ktor.server.application.*
+import big.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
